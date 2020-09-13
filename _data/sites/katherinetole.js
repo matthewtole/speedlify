5d2ddb9cd7837fee04df3e1cd3bb2e14eff25725
Matthew Tole <matthewtole@gmail.com>
2020-09-12 17:01:16 -0700

    Configure the sites

---
+++ _data/sites/katherinetole.js
@@ -0,0 +1,18 @@
+module.exports = {
+  name: "katherintole.com", // optional, falls back to object key
+  description: "Katherine Tole's website",
+  // skip if localhost
+  // skip if this is a new fork of the speedlify (not Zach’s)
+  // skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    // Use "run" if the sites don’t share assets on the same origin
+    //           and we can reset chrome with each run instead of
+    //           each site in every run (it’s faster)
+    // Use "site" if sites are all on the same origin and share assets.
+    freshChrome: "site",
+  },
+  urls: [
+    "https://katherinetole.com/",
+  ],
+};
