d72ffc1e09f205e59eb3850872dad6eb15cb91ea
Matthew Tole <matthewtole@gmail.com>
2021-01-17 22:28:28 -0800

    Create marymarquiss.js

---
+++ _data/sites/marymarquiss.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "marymarquiss.com", // optional, falls back to object key
+  description: "Mary Marquiss",
+  options: {
+    frequency: 60 * 23, // 23 hours
+    freshChrome: "site",
+  },
+  urls: [
+    "https://marymarquiss.netlify.com/",
+  ],
+};
